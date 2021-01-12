c870dad65d1c305bf5e469c60fa72183cf5f1273
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-11 18:40:26 -0700

    thoughts

---
+++ controller/thought-controller.js
@@ -0,0 +1,49 @@
+const { Thought } = require("../models");
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+      Thought.find({})
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+    getThoughtById({ params }, res) {
+      Thought.findOne({ _id: params.id })
+        .populate("users", "friends")
+        .select("-__v")
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+    createThought({ body }, res) {
+      Thought.create(body)
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+    },
+  
+    updateThought({ params, body }, res) {
+      Thought.findOneAndUpdate({ _id: params.id }, body, {
+        new: true,
+        runValidators: true,
+      })
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            res.status(404).json({ message: "No Thought found with this id!" });
+            return;
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
+    deleteThought({ params }, res) {
+      Thought.findOneAndDelete({ _id: params.id })
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+    },
+  };
+  
+  module.exports = thoughtController;
--- routes/thoughts-routes.js
+++ routes/thoughts-routes.js
@@ -1,17 +1,19 @@
 const router = require("express").Router();
 
-const { Thoughts } = require("../models");
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../controller/thought-controller");
 
-router.get("/", (req, res) => {
-    User.findAll({
-      // attributes: { exclude: ["password"] },
-    })
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.route("/").get(getAllThoughts).post(createThought);
 
- 
-  
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
