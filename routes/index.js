72f6674a3d2c6ff49e1b58c2934f053bed6adcc4
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-13 20:01:45 -0700

    hopefully

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const thoughtRoutes = require("./thought-routes");
 // const reactionRoutes = require("./reaction-routes");
 
 router.use('/api/user', userRoutes);
-router.use('/api/thoughts', thoughtRoutes);
+router.use('/api/thought', thoughtRoutes);
 // router.use('/api/reaction', reactionRoutes);
 
 module.exports = router;
